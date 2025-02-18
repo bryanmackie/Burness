f22b6ceccc8187bdb41fdcf8e6fe1e29c287730d
bryanmackie <bryanmackie7@gmail.com>
2025-02-18 16:07:16 -0500

    Update server.js

--- EmployeeCompTracker/src/server.js
+++ EmployeeCompTracker/src/server.js
@@ -45,9 +45,11 @@ app.post('/verify-passphrase', async (req, res) => {
   if (passphrase === validPassphrase) {
     role = 'manager';
     employeesQuery = 'SELECT first_name, last_name, id FROM employee_salary WHERE access_group != $1';
+    res.json({ success: true, message: 'Passphrase correct. You have access.' });
   } else if (passphrase === validPassphraseAdmin) {
     role = 'admin';
     employeesQuery = 'SELECT first_name, last_name, id FROM employee_salary';
+    res.json({ success: true, message: 'Passphrase correct. You have access.' });
   } else {
     return res.status(403).json({ success: false, message: 'Incorrect passphrase. Access denied.' });
   }
