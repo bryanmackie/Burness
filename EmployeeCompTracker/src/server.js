fc346fc8601ad00d6081572a603c4f2a2e8f99fe
bryanmackie <bryanmackie7@gmail.com>
2025-02-26 18:46:50 -0500

    Update server.js

--- EmployeeCompTracker/src/server.js
+++ EmployeeCompTracker/src/server.js
@@ -13,10 +13,13 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 // Serve static files from the "dist" folder after build
-app.use(express.static(path.join(__dirname, 'dist')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+app.use(express.static(path.join(__dirname, 'dist', 'src')));
+
+// Serve the index.html from 'dist/src/index.html'
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'src', 'index.html'));
 });
+
 // Parse JSON request bodies
 app.use(bodyParser.json());
 
