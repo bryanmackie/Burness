6f29edfb3eeaa037732ed68c474ecc163deec8e4
bryanmackie <bryanmackie7@gmail.com>
2025-02-26 14:24:57 -0500

    Update vite.config.js

--- EmployeeCompTracker/vite.config.js
+++ EmployeeCompTracker/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: './',  // Serve from the root directory
   build: {
-    outDir: './dist',  // Build files into the 'dist' folder
+    outDir: 'dist',  // Build files into the 'dist' folder
     rollupOptions: {
       input: {
         main: 'src/index.html',  // Entry HTML file in the 'src' folder
