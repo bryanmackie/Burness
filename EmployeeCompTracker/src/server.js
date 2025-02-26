3f56b0716b9eeff80383f5bcf5d6da8a2e8be699
bryanmackie <bryanmackie7@gmail.com>
2025-02-26 18:27:10 -0500

    Attempt

--- EmployeeCompTracker/src/server.js
+++ EmployeeCompTracker/src/server.js
@@ -13,8 +13,10 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 // Serve static files from the "dist" folder after build
-app.use(express.static(path.join(__dirname, '../dist')));
-
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 // Parse JSON request bodies
 app.use(bodyParser.json());
 
