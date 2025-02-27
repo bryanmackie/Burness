482be32b28852bfd01ed8178520ff055bbfe3753
bryanmackie <bryanmackie7@gmail.com>
2025-02-27 11:22:36 -0500

    D3

--- EmployeeCompTracker/src/app.js
+++ EmployeeCompTracker/src/app.js
@@ -1,20 +1,20 @@
 import * as d3 from 'd3';
 
-// Create a simple bar chart
-const data = [30, 50, 80, 120, 150];
-
-const width = 500;
-const height = 300;
-
-const svg = d3.select('body').append('svg')
-  .attr('width', width)
-  .attr('height', height);
-
-svg.selectAll('rect')
-  .data(data)
-  .enter().append('rect')
-  .attr('x', (d, i) => i * 100)  // Position each bar
-  .attr('y', d => height - d)    // Height of each bar
-  .attr('width', 80)
-  .attr('height', d => d)
-  .attr('fill', 'blue');
+export function createChart() {
+    // Your D3 code here
+    console.log('Creating D3 chart...');
+    // Example: create a simple bar chart
+    d3.select('body')
+        .append('svg')
+        .attr('width', 400)
+        .attr('height', 200)
+        .selectAll('rect')
+        .data([50, 100, 150, 200])
+        .enter()
+        .append('rect')
+        .attr('width', 50)
+        .attr('height', d => d)
+        .attr('x', (d, i) => i * 60)
+        .attr('y', d => 200 - d)
+        .attr('fill', 'blue');
+}
