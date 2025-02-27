52249c3aeafce17fcac31c80215b2bd51c2d3d3a
bryanmackie <bryanmackie7@gmail.com>
2025-02-27 11:00:26 -0500

    Update app.js

--- EmployeeCompTracker/src/app.js
+++ EmployeeCompTracker/src/app.js
@@ -1,14 +1,20 @@
 import * as d3 from 'd3';
 
+// Create a simple bar chart
+const data = [30, 50, 80, 120, 150];
+
 const width = 500;
-const height = 500;
+const height = 300;
 
 const svg = d3.select('body').append('svg')
   .attr('width', width)
   .attr('height', height);
 
-svg.append('circle')
-  .attr('cx', width / 2)
-  .attr('cy', height / 2)
-  .attr('r', 50)
+svg.selectAll('rect')
+  .data(data)
+  .enter().append('rect')
+  .attr('x', (d, i) => i * 100)  // Position each bar
+  .attr('y', d => height - d)    // Height of each bar
+  .attr('width', 80)
+  .attr('height', d => d)
   .attr('fill', 'blue');
