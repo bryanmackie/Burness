aec37946c7b730e50308404e811154e990f6e14d
bryanmackie <bryanmackie7@gmail.com>
2025-03-04 12:01:31 -0500

    Attempt2

--- EmployeeCompTracker/public/app.js
+++ EmployeeCompTracker/public/app.js
@@ -1,6 +1,6 @@
 // app.js
-import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.8.0/dist/d3.min.js';
 
+import * as d3 from 'https://cdnjs.cloudflare.com/ajax/libs/d3/7.9.0/d3.min.js';
 export function createChart() {
   const chartContainer = document.getElementById("chartContainer");
   console.log("Chart Container:", chartContainer);
